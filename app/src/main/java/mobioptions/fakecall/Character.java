09df7a4ed2bd517216a6810208234843958d0124
ayoub00xyz <ay.khadrani@gmail.com>
2023-09-06 12:47:28 +0100

    Initial commit

---
+++ app/src/main/java/mobioptions/fakecall/Character.java
@@ -0,0 +1,15 @@
+package mobioptions.fakecall;
+
+public class Character {
+    int imageResource;
+    String name;
+    String number;
+    int audioResource;
+
+    Character(int imageResource, String name, String number, int audioResource) {
+        this.imageResource = imageResource;
+        this.name = name;
+        this.number = number;
+        this.audioResource = audioResource;
+    }
+}
---
+++ app/src/main/java/mobioptions/fakecall/SplashScreen.java
@@ -0,0 +1,35 @@
+package mobioptions.fakecall;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView logo = findViewById(R.id.splash_logo);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+        logo.startAnimation(animation);
+
+        // Delay for the splash screen
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, Choose_Category.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // 3 seconds
+    }
+}
